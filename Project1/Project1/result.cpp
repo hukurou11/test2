8ea777a2c6bc9c798ccbc48341b0fb4eb3ac80be
吉澤弦気 <humihumi.27.kisaragi@gmail.com>
2020-08-11 01:15:36 +0900

    change pass title→game→resule and little edit camera.cpp

--- Project1/Project1/result.cpp
+++ Project1/Project1/result.cpp
@@ -6,10 +6,15 @@
 
 void CResult::Init()
 {
-
+	AddGameObject<CCamera>(0);
 }
 
 void CResult::Update()
 {
+	CScene::Update();
 
+	if (CInput::GetKeyTrigger(VK_RETURN))
+	{
+		CManager::SetScene<CTitle>();
+	}
 }
